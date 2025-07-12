977d2694d73db15e982f693f6afdcde8a34226bf
penguinissus <jocelyn.ty.lau@gmail.com>
2025-07-12 12:14:28 -0400

    tutorial part 6

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const input = document.getElementById('input');
 const color_picker = document.getElementById('color');
 const vol_slider = document.getElementById('vol-slider');
+const recording_toggle = document.getElementById('record');
 
 //create web audio api elements
 const audioCtx = new AudioContext();
@@ -19,13 +20,21 @@ gainNode.gain.value = 0;
 
 //create a map of notes
 notenames = new Map();
-notenames.set("C",261.6);
-notenames.set("D",293.7);
-notenames.set("E",329.6);
-notenames.set("F",349.2);
-notenames.set("G",392.0);
-notenames.set("A",440);
-notenames.set("B",493.9);
+notenames.set("c",261.6);
+notenames.set("d",293.7);
+notenames.set("e",329.6);
+notenames.set("f",349.2);
+notenames.set("g",392.0);
+notenames.set("a",440);
+notenames.set("b",493.9);
+notenames.set("C",(261.6*2));
+notenames.set("D",(293.7*2));
+notenames.set("E",(329.6*2));
+notenames.set("F",(349.2*2));
+notenames.set("G",(392*2));
+notenames.set("A",(880));
+notenames.set("B",(493.9*2));
+notenames.set("X",0);
 
 //define canvas variables
 var canvas = document.getElementById("canvas");
@@ -108,4 +117,46 @@ function handle(){
             clearInterval(repeat)
         }
     }, timepernote);
+}
+
+var blob, recorder = null;
+var chunks = [];
+function startRecording(){
+    const canvasStream = canvas.captureStream(20); //frame rate of canvas
+    const audioDestination = audioCtx.createMediaStreamDestination();
+    gainNode.connect(audioDestination);
+    const combinedStream = new MediaStream();
+    canvasStream.getVideoTracks().forEach(track => combinedStream.addTrack(track));
+    audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
+    recorder = new MediaRecorder(combinedStream, {mimeType: 'video/webm'});
+
+    recorder.ondataavailable = e => {
+        if(e.data.size > 0){
+            chunks.push(e.data);
+        }
+    };
+
+    recorder.onstop = () => {
+        const blob = new Blob(chunks, {type: 'video/webm'});
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recording.webm';
+        a.click();
+        URL.revokeObjectURL(url);
+    };
+
+    recorder.start();
+}
+
+var is_recording = false;
+function toggle(){
+    is_recording = !is_recording;
+    if(is_recording){
+        recording_toggle.innerHTML = "Stop Recording";
+        startRecording();
+    } else {
+        recording_toggle.innerHTML = "Start Recording";
+        recorder.stop();
+    }
 }
